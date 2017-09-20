0df9ea215cffab973eb3fa81969fdc0b4e51183e
Ahmer Malik <ahmer9800@gmail.com>
2017-09-20 14:17:36 -0500

    updated code

--- hero_rpg.py
+++ hero_rpg.py
@@ -8,11 +8,46 @@
 
 #4. This is a test.
 
+
+class Character():
+    def __init__(self, name, health, power):
+        self.name = name
+        self.health = health
+        self.power = power
+
+class Hero(Character):
+    def __init__(self, name = 'Hero', health=10, power=5):
+        self.name = name
+        self.heath = health
+        self.power = power
+
+
+    def attack(self, enemy):
+            # Hero attacks goblin
+        enemy.health -= self.power
+        print("You do {} damage to the goblin.".format(self.power))
+
+
+
+
+class Goblin(Character):
+    def ___init__(self, name = 'Goblin', health=6, power=2):
+        self.name = name
+        self.health = health
+        self.power = power
+
+
+
+
+
+
 def main():
     hero_health = 10
     hero_power = 5
     goblin_health = 6
     goblin_power = 2
+    hero = Hero('Hero', 10, 5)
+    goblin = Goblin('Goblin', 6, 2)
 
     while goblin_health > 0 and hero_health > 0:
         print("You have {} health and {} power.".format(hero_health, hero_power))
@@ -26,8 +61,9 @@ def main():
         raw_input = input()
         if raw_input == "1":
             # Hero attacks goblin
-            goblin_health -= hero_power
-            print("You do {} damage to the goblin.".format(hero_power))
+            # goblin_health -= hero_power
+            # print("You do {} damage to the goblin.".format(hero_power))
+            hero.attack(goblin)
             if goblin_health <= 0:
                 print("The goblin is dead.")
         elif raw_input == "2":
